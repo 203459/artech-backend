c229874eac87b1bd272e89a0eb256b353b8e227f
203459 <203459@ids.upchiapas.edu.mx>
2023-12-06 16:02:47 -0600

    modificate

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import Database from './database/db';
+import express, { Application, Request, Response } from "express";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+    this.databaseSync();
+    this.plugins();
+    this.routes();
+  }
+
+  protected plugins(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  protected databaseSync(): void {
+    const db = new Database();
+    db.sequelize?.sync();
+  }
+
+  protected routes(): void {
+    this.app.route("/").get((req: Request, res: Response) => {
+      res.send("welcome home");
+    });
+    this.app.use('/api/v1/user', chatRouter);
+  }
+}
+
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+new App().app.listen(SERVER_PORT, () => {
+  console.log(`✅ Server run in port ${SERVER_PORT}`);
+});
